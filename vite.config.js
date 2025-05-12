da468f8d9b44b1c80854c28ad667250a65f838ad
katwalreejan <Reejan.Katwal@metropolia.fi>
2025-05-12 16:11:27 +0545

    Deployed project on github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/week4-lab5part2/", 
   plugins: [react()],
-})
+});
