e13baf640bbce7a3d25e18aeac67754a341f95c1
katwalreejan <Reejan.Katwal@metropolia.fi>
2025-05-12 15:45:22 +0545

    Created components , converted html section to jsx and organize it

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import MainContent from "./MainContent";
+import "./styles.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Copyright © 2024 My JSX App</p>
+      <p>Design</p>
+    </div>
+  );
+};
+
+export default Footer;
